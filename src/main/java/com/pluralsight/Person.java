4a632257351a1be76325b7f2cd45a97c2d13e584
RickelviC <Cruzrickelvi@gmail.com>
2026-05-14 14:47:38 -0400

    added logic

--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        int last = this.lastName.compareTo(person.lastName);
+        return last == 0 ? this.firstName.compareTo(person.firstName) : last;
+    }
+
+    @Override
+    public String toString() {
+        return "first Name: " + firstName + ", last Name: " + lastName + ", age: " + age;
+    }
 }
